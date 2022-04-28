7ec1d9769804fb7de0e14b9d6da0c371a0dca065
Richard Aiello <richaiello17@gmail.com>
2022-04-28 10:23:06 -0400

    Formatted files in src/ folder.

--- src/booking.js
+++ src/booking.js
@@ -1,152 +1,149 @@
-import JustValidate from 'just-validate';
+import JustValidate from "just-validate";
 
 // date validation variables
-const months30days = ['4', '6', '9', '11'];
-const monthInput = document.querySelector('#month');
-const dayInput = document.querySelector('#day');
+const months30days = ["4", "6", "9", "11"];
+const monthInput = document.querySelector("#month");
+const dayInput = document.querySelector("#day");
 
 // date-time variables
-const dateTimeInputs = document.querySelectorAll('.date-time');
+const dateTimeInputs = document.querySelectorAll(".date-time");
 
 // customer total variables
-const subtractBtn = document.querySelector('.btn--customer-subtract');
-const addBtn = document.querySelector('.btn--customer-add');
-const customerTotal = document.querySelector('.customer-total');
+const subtractBtn = document.querySelector(".btn--customer-subtract");
+const addBtn = document.querySelector(".btn--customer-add");
+const customerTotal = document.querySelector(".customer-total");
 
-// Global variable for max days to be used with form validation and listeners 
+// Global variable for max days to be used with form validation and listeners
 let maxDays = 31;
 
 /**********************************************
     FORM VALIDATION
 **********************************************/
 // Implements JustValidate to add custom form validation
-const validation = new JustValidate(
-    '#form',
-    {
-        errorFieldCssClass: 'invalid',
-        errorLabelCssClass: 'invalid-label',
-        focusInvalidField: true,
-        errorsContainer: 'div.invalid-container'
-    }
-);
+const validation = new JustValidate("#form", {
+  errorFieldCssClass: "invalid",
+  errorLabelCssClass: "invalid-label",
+  focusInvalidField: true,
+  errorsContainer: "div.invalid-container",
+});
 
 validation
-    .addField('#name', [
-        {   
-            rule: 'required',
-            errorMessage: 'Name is required',
-        },
-        {
-            rule: 'customRegexp',
-            value: /(^[A-Za-z]*$)/,
-            errorMessage: 'Name may only contain letters',
-        },
-        {
-            rule: 'minLength',
-            value: 3,
-            errorMessage: 'Name must contain a minimum of 3 characters',
-        },
-        {
-            rule: 'maxLength',
-            value: 30,
-            errorMessage: 'Name must contain a maximum of 30 characters',
-        },
-    ])
-    .addField('#email', [
-        {
-            rule: 'required',
-            errorMessage: 'Email is required',
-        },
-        {
-            rule: 'email',
-            errorMessage: 'Please enter a valid email address (******@gmail.com)',
-        },
-    ])
-    .addField('#month', [
-        {
-            rule: 'required',
-            errorMessage: 'Please enter a month',
-        },
-        {
-            rule: 'minNumber',
-            value: 1,
-            errorMessage: 'Month should be at least 01',
-        },
-        {
-            rule: 'maxNumber',
-            value: 12,
-            errorMessage: 'Month should be at most 12',
-        }
-    ])
-    .addField('#day', [
-        {
-            rule: 'required',
-            errorMessage: 'Please enter a day',
-        },
-        {
-            rule: 'minNumber',
-            value: 1,
-            errorMessage: 'Day should be atleast 01',
-        },
-        {
-            validator: () => maxDays >= dayInput.value,
-            errorMessage: 'Please enter a valid day for the corresponding month',
-        }
-    ])
-    .addField('#year', [
-        {
-            rule: 'required',
-            errorMessage: 'Please choose a year from 2022 to 2024',
-        },
-        {
-            rule: 'minNumber',
-            value: 2022,
-            errorMessage: 'Year should be at least 2022',
-        },
-        {
-            rule: 'maxNumber',
-            value: 2024,
-            errorMessage: 'Month should be at most 2024',
-        }
-    ])
-    .addField('#hour', [
-        {
-            rule: 'required',
-            errorMessage: 'Hour field is required',
-        },
-        {
-            rule: 'minNumber',
-            value: 1,
-            errorMessage: 'Hour should be at least 01',
-        },
-        {
-            rule: 'maxNumber',
-            value: 12,
-            errorMessage: 'Hour should be at most 12',
-        }
-    ])
-    .addField('#minute', [
-        {
-            rule: 'required',
-            errorMessage: 'Minute field is required',
-        },
-        {
-            rule: 'minNumber',
-            value: 0,
-            errorMessage: 'Minute should be at least 00',
-        },
-        {
-            rule: 'maxNumber',
-            value: 59,
-            errorMessage: 'Minute should be at most 59',
-        }
-    ])
-    .onFail(e => {
-        console.log('Invalid', e);
-    })
-    .onSuccess(e => {
-        console.log('Passed', e);
-    })
+  .addField("#name", [
+    {
+      rule: "required",
+      errorMessage: "Name is required",
+    },
+    {
+      rule: "customRegexp",
+      value: /(^[A-Za-z]*$)/,
+      errorMessage: "Name may only contain letters",
+    },
+    {
+      rule: "minLength",
+      value: 3,
+      errorMessage: "Name must contain a minimum of 3 characters",
+    },
+    {
+      rule: "maxLength",
+      value: 30,
+      errorMessage: "Name must contain a maximum of 30 characters",
+    },
+  ])
+  .addField("#email", [
+    {
+      rule: "required",
+      errorMessage: "Email is required",
+    },
+    {
+      rule: "email",
+      errorMessage: "Please enter a valid email address (******@gmail.com)",
+    },
+  ])
+  .addField("#month", [
+    {
+      rule: "required",
+      errorMessage: "Please enter a month",
+    },
+    {
+      rule: "minNumber",
+      value: 1,
+      errorMessage: "Month should be at least 01",
+    },
+    {
+      rule: "maxNumber",
+      value: 12,
+      errorMessage: "Month should be at most 12",
+    },
+  ])
+  .addField("#day", [
+    {
+      rule: "required",
+      errorMessage: "Please enter a day",
+    },
+    {
+      rule: "minNumber",
+      value: 1,
+      errorMessage: "Day should be atleast 01",
+    },
+    {
+      validator: () => maxDays >= dayInput.value,
+      errorMessage: "Please enter a valid day for the corresponding month",
+    },
+  ])
+  .addField("#year", [
+    {
+      rule: "required",
+      errorMessage: "Please choose a year from 2022 to 2024",
+    },
+    {
+      rule: "minNumber",
+      value: 2022,
+      errorMessage: "Year should be at least 2022",
+    },
+    {
+      rule: "maxNumber",
+      value: 2024,
+      errorMessage: "Month should be at most 2024",
+    },
+  ])
+  .addField("#hour", [
+    {
+      rule: "required",
+      errorMessage: "Hour field is required",
+    },
+    {
+      rule: "minNumber",
+      value: 1,
+      errorMessage: "Hour should be at least 01",
+    },
+    {
+      rule: "maxNumber",
+      value: 12,
+      errorMessage: "Hour should be at most 12",
+    },
+  ])
+  .addField("#minute", [
+    {
+      rule: "required",
+      errorMessage: "Minute field is required",
+    },
+    {
+      rule: "minNumber",
+      value: 0,
+      errorMessage: "Minute should be at least 00",
+    },
+    {
+      rule: "maxNumber",
+      value: 59,
+      errorMessage: "Minute should be at most 59",
+    },
+  ])
+  .onFail((e) => {
+    console.log("Invalid", e);
+  })
+  .onSuccess((e) => {
+    console.log("Passed", e);
+  });
 
 /**********************************************
     EVENT LISTENERS
@@ -154,62 +151,52 @@ validation
 // Listens for change in the month field
 // Determines the max attribute value for the days field based on the current month
 // Updates the global maxDays value based on the current month
-monthInput.addEventListener('change', e => {
-
-    if (months30days.includes(monthInput.value)) {
-
-        dayInput.setAttribute('max', '30');
-        maxDays = 30;
-
-    } else if (monthInput.value === "2") {
-
-        dayInput.setAttribute('max', '28');
-        maxDays = 28;
-
-    } else {
-
-        dayInput.setAttribute('max', '31');
-        maxDays = 31;
-    }
+monthInput.addEventListener("change", (e) => {
+  if (months30days.includes(monthInput.value)) {
+    dayInput.setAttribute("max", "30");
+    maxDays = 30;
+  } else if (monthInput.value === "2") {
+    dayInput.setAttribute("max", "28");
+    maxDays = 28;
+  } else {
+    dayInput.setAttribute("max", "31");
+    maxDays = 31;
+  }
 });
 
 // Two digit appearence for date-time fields
-dateTimeInputs.forEach(input => {
+dateTimeInputs.forEach((input) => {
+  input.addEventListener("change", (e) => {
+    const value = Number.parseInt(e.currentTarget.value, 10);
 
-    input.addEventListener('change', e => {
-
-        const value =  Number.parseInt(e.currentTarget.value, 10);
-    
-        if (value < 10) {
-            e.currentTarget.value = `0${value}`;
-        }
-    });
+    if (value < 10) {
+      e.currentTarget.value = `0${value}`;
+    }
+  });
 });
 
 // Displays the total customers when booking a reservation
 // Subtracts from total customers on click
-subtractBtn.addEventListener('click', e => {
+subtractBtn.addEventListener("click", (e) => {
+  let total = Number.parseInt(customerTotal.value, 10);
 
-    let total = Number.parseInt(customerTotal.value, 10);
+  if (total > 1) {
+    total--;
+    customerTotal.value = `${total} People`;
+  }
 
-    if (total > 1) {
-        total--;
-        customerTotal.value = `${total} People`;
-    } 
-    
-    if (total === 1) {
-        customerTotal.value = `1 Person`;
-    }
+  if (total === 1) {
+    customerTotal.value = `1 Person`;
+  }
 });
 
 // Displays the total customers when booking a reservation
 // Adds to total customers on click
-addBtn.addEventListener('click', e => {
+addBtn.addEventListener("click", (e) => {
+  let total = Number.parseInt(customerTotal.value, 10);
 
-    let total = Number.parseInt(customerTotal.value, 10);
-
-    if (total < 30) {
-        total++;
-        customerTotal.value = `${total} People`;
-    }
+  if (total < 30) {
+    total++;
+    customerTotal.value = `${total} People`;
+  }
 });
--- src/index.js
+++ src/index.js
@@ -1,144 +1,130 @@
 // Variables for section events
-const eventsImages = document.querySelectorAll('.image--events');
-const eventsBtns = document.querySelectorAll('.btn--events');
-const eventsAccents = document.querySelectorAll('.btn--events .accent');
-const eventsHeading = document.querySelector('.events__heading');
-const eventsText = document.querySelector('.events__text');
+const eventsImages = document.querySelectorAll(".image--events");
+const eventsBtns = document.querySelectorAll(".btn--events");
+const eventsAccents = document.querySelectorAll(".btn--events .accent");
+const eventsHeading = document.querySelector(".events__heading");
+const eventsText = document.querySelector(".events__text");
 
 // Varibales for family gathering button
-const familyBtn = document.querySelector('.btn--events.family');
+const familyBtn = document.querySelector(".btn--events.family");
 const familyHeading = `Family Gathering`;
-const familyText = 
-    `We love catering for entire families. So please 
+const familyText = `We love catering for entire families. So please 
     bring everyone along for a special meal with your 
     loved ones. We’ll provide a memorable experience for all.`;
 
 // Variables for special events button
-const specialBtn = document.querySelector('.btn--events.special');
+const specialBtn = document.querySelector(".btn--events.special");
 const specialHeading = `Special Events`;
-const specialText = 
-    `Whether it’s a romantic dinner or special date you’re 
+const specialText = `Whether it’s a romantic dinner or special date you’re 
     celebrating with others we’ll look after you. We’ll be 
     sure to mark your special date with an unforgettable meal.`;
 
 // Variables for social events button
-const socialBtn = document.querySelector('.btn--events.social');
+const socialBtn = document.querySelector(".btn--events.social");
 const socialHeading = `Social Events`;
-const socialText = 
-    `Are you looking to have a larger social event? No problem! 
+const socialText = `Are you looking to have a larger social event? No problem! 
     We’re more than happy to cater for big parties. We’ll work 
     with you to make your event a hit with everyone.`;
 
 // Checks each image in events for the provided class name.
 // Uses the ternary operator to decide the display status of each image.
-const imageContainsClass = (string => {
+const imageContainsClass = (string) => {
+  eventsImages.forEach((image) => {
+    const classes = image.classList;
 
-    eventsImages.forEach(image => {
-        const classes = image.classList;
-        
-        classes.contains(string) ? classes.remove('hidden') : classes.add('hidden');
-    });
-});
+    classes.contains(string) ? classes.remove("hidden") : classes.add("hidden");
+  });
+};
 
 // Checks each button in events for the provided class name.
 // Uses the ternary operator to decide the active state of each button.
-const btnContainsClass = (string => {
+const btnContainsClass = (string) => {
+  eventsBtns.forEach((btn) => {
+    const classes = btn.classList;
 
-    eventsBtns.forEach(btn => {
-        const classes = btn.classList;
-        
-        classes.contains(string) ? classes.add('active') : classes.remove('active');
-    });
-});
+    classes.contains(string) ? classes.add("active") : classes.remove("active");
+  });
+};
 
 // Checks each accent in events for the provided class name.
 // Uses the ternary operator to decide the display status of each accent.
-const accentContainsClass = (string => {
+const accentContainsClass = (string) => {
+  eventsAccents.forEach((accent) => {
+    const classes = accent.classList;
 
-    eventsAccents.forEach(accent => {
-        const classes = accent.classList;
-        
-        classes.contains(string) ? classes.remove('hidden') : classes.add('hidden');
-    });
-});
+    classes.contains(string) ? classes.remove("hidden") : classes.add("hidden");
+  });
+};
 
 // Listens for the end of animation on events heading and text
 // Animates events heading and text by adding a class containing keyframes
 // Removes keyframes class at the end of animation
 const animateContext = () => {
+  eventsHeading.addEventListener("animationend", (e) => {
+    e.currentTarget.classList.remove("slide");
+  });
 
-    eventsHeading.addEventListener('animationend', e => {
-        e.currentTarget.classList.remove('slide');
-    });
+  eventsText.addEventListener("animationend", (e) => {
+    e.currentTarget.classList.remove("slide");
+  });
 
-    eventsText.addEventListener('animationend', e => {
-        e.currentTarget.classList.remove('slide');
-    });
-
-    eventsHeading.classList.add('slide');
-    eventsText.classList.add('slide');
-}
+  eventsHeading.classList.add("slide");
+  eventsText.classList.add("slide");
+};
 
 // Disables all buttons for section events
 // Enables all buttons for section events after 500ms to allow animations to finish.
 // Changes content of events heading and text
 const changeContext = (heading, text) => {
+  eventsBtns.forEach((btn) => btn.setAttribute("disabled", "disabled"));
 
-    eventsBtns.forEach(btn => btn.setAttribute('disabled', 'disabled'));
+  setTimeout(() => {
+    eventsBtns.forEach((btn) => btn.removeAttribute("disabled"));
+  }, "500");
 
-    setTimeout(() => {
-        eventsBtns.forEach(btn => btn.removeAttribute('disabled'));
-    }, "500");
-
-    eventsHeading.textContent = heading;
-    eventsText.textContent = text;
-}
+  eventsHeading.textContent = heading;
+  eventsText.textContent = text;
+};
 
 /**********************************************
     EVENT LISTENERS
 **********************************************/
 
 // Family Gathering button
-familyBtn.addEventListener('click', e => {
+familyBtn.addEventListener("click", (e) => {
+  if (eventsHeading.textContent !== familyHeading) {
+    imageContainsClass("family");
+    btnContainsClass("family");
+    accentContainsClass("accent--family");
 
-    if (eventsHeading.textContent !== familyHeading) {
-
-        imageContainsClass('family');
-        btnContainsClass('family');
-        accentContainsClass('accent--family');
-
-        // animateContext is placed above changeContext to ensure condition functionality
-        animateContext();
-        changeContext(familyHeading, familyText);  
-    }
+    // animateContext is placed above changeContext to ensure condition functionality
+    animateContext();
+    changeContext(familyHeading, familyText);
+  }
 });
 
 // Special Events button
-specialBtn.addEventListener('click', e => {
+specialBtn.addEventListener("click", (e) => {
+  if (eventsHeading.textContent !== specialHeading) {
+    imageContainsClass("special");
+    btnContainsClass("special");
+    accentContainsClass("accent--special");
 
-    if (eventsHeading.textContent !== specialHeading) {
-
-        imageContainsClass('special');
-        btnContainsClass('special');
-        accentContainsClass('accent--special');
-
-        // animateContext is placed above changeContext to ensure condition functionality
-        animateContext();
-        changeContext(specialHeading, specialText);
-    }
+    // animateContext is placed above changeContext to ensure condition functionality
+    animateContext();
+    changeContext(specialHeading, specialText);
+  }
 });
 
 // Social Events button
-socialBtn.addEventListener('click', e => {
+socialBtn.addEventListener("click", (e) => {
+  if (eventsHeading.textContent !== socialHeading) {
+    imageContainsClass("social");
+    btnContainsClass("social");
+    accentContainsClass("accent--social");
 
-    if (eventsHeading.textContent !== socialHeading) {
-
-        imageContainsClass('social');
-        btnContainsClass('social');
-        accentContainsClass('accent--social');
-
-        // animateContext is placed above changeContext to ensure condition functionality
-        animateContext();
-        changeContext(socialHeading, socialText);
-    }
+    // animateContext is placed above changeContext to ensure condition functionality
+    animateContext();
+    changeContext(socialHeading, socialText);
+  }
 });
